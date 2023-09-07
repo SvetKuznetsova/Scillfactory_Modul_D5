694421e44f4506acbb54346b76363daebebe1119
SvetKuznetsova <122929668+SvetKuznetsova@users.noreply.github.com>
2023-09-07 22:09:19 +0300

    Add files via upload

--- NewsPaper/protect/views.py
+++ NewsPaper/protect/views.py
@@ -6,16 +6,17 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class IndexView(LoginRequiredMixin, TemplateView):
     template_name = 'protect/index.html'
+    context_object_name = 'protect'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['is_not_author'] = not self.request.user.groups.filter(name='author').exists()
+        context['is_not_author'] = not self.request.user.groups.filter(name='authors').exists()
         return context
 
 @login_required
 def upgrade_me(request):
    user = request.user
-   author_group = Group.objects.get(name='author')
-   if not request.user.groups.filter(name='author').exists():
+   author_group = Group.objects.get(name='authors')
+   if not request.user.groups.filter(name='authors').exists():
        author_group.user_set.add(user)
    return redirect('/')
